0428b45697fe466a3e815981eaa18c3135801874
Sohel Daniel <sohel.danial@campus.technion.ac.il>
2026-09-25 03:55:52 +0300

    Mesh.cpp created

--- Mesh.cpp
+++ Mesh.cpp
@@ -1,1 +1,31 @@
 #include "Mesh.h"
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+Mesh::Mesh(std::string& filename){
+	std::ifstream file(filename);
+	if(!file){
+		throw std::runtime_error("Could not open file: " + filename +"\n"std::endl;
+	}
+	std::string line;
+	while(std::getline(file,line)){
+		std::istringstream iss(line);
+
+		if(line.compare(0,2,"v ") == 0){
+			std::string tag;
+			float a,b,c;
+			iss>>tag>>a>>b>>c;
+			Vec3 temp = {a,b,c};
+			vertices.push_back(temp);
+		}
+		else if(line.compare(0,2,"f ") == 0){
+			std::string tag,t1,t2;
+			int a,b,c;
+			iss>>tag>>a>>b>>c;
+			Triangle temp = {a,b,c};
+			triangles.push_back(temp);
+		}
+	}
+	file.close();
+
+}
